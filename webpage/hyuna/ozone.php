7ac0fe82eb2989e8a59d9d35242dde864a96388d
Hyuna Kim <akxmcse@gmail.com>
2022-11-16 03:01:31 +0900

    [FEAT] 오존 롤업 구현

--- webpage/hyuna/ozone.php
+++ webpage/hyuna/ozone.php
@@ -39,20 +39,26 @@
 
             <?php
             $conn = mysqli_connect("localhost", "team20", "team20", "weather");
-            $query = "SELECT CITY, AVG_OZONE 
+            $query1 = "SELECT CITY, AVG_OZONE 
             ,RANK() OVER(ORDER BY AVG_OZONE) AS RANKING
             FROM AVG_OZONE";
-            $result = mysqli_query($conn, $query);
+            $result1 = mysqli_query($conn, $query1);
+
+            $query2 = "SELECT DATES, CITY, AVG_OZONE
+            FROM AVG_OZONE_BY_DATES
+            ORDER BY CITY";
+            $result2 = mysqli_query($conn, $query2);
             ?>
 
             <FORM>
+                <p>Ozone Ranking of October</p>
                 <TABLE Border=1>
                     <tr>
                         <td>Ranking</td>
                         <td>City</td>
                         <td>Average Ozone</td>
                     </tr>
-                    <?php while ($row = mysqli_fetch_array($result)) { ?>
+                    <?php while ($row = mysqli_fetch_array($result1)) { ?>
                     <tr>
                         <td>
                             <?= $row['RANKING'] ?>
@@ -66,10 +72,34 @@
                     </tr>
                     <?php } ?>
                 </TABLE>
+
+                <p>Ozone concentration trend of October (Blank means 'All')</p>
+                <TABLE Border=1>
+                    <tr>
+                        <td>City</td>
+                        <td>Dates</td>
+                        <td>Average ozone</td>
+                    </tr>
+                    <?php while ($row = mysqli_fetch_array($result2)) { ?>
+                    <tr>
+                        <td>
+                            <?= $row['CITY'] ?>
+                        </td>
+                        <td>
+                            <?= $row['DATES'] ?>
+                        </td>
+                        <td>
+                            <?= $row['AVG_OZONE'] ?>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </TABLE>
+
             </FORM>
 
             <?php
-            mysqli_free_result($result);
+            mysqli_free_result($result1);
+            mysqli_free_result($result2);
             mysqli_close($conn);
             ?>
         </div>
