c700dc2bcd49529b88ec041371f195d302c98143
Hyuna Kim <akxmcse@gmail.com>
2022-11-15 17:51:23 +0900

    폴더링 작업 및 sql문 추가

--- sql/create.sql
+++ sql/create.sql
@@ -8,19 +8,18 @@ create table temperature(
     primary key (mdate, region_code), 
     foreign key (region_code) references region);
 
-/*YEONHUI*/
 create table typhoon(
     typhoon_name varchar(20),
     country varchar(20),
     used boolean default 0, 
     primary key (typhoon_name));
 
-/*YEONHUI*/
 create table new_typhoon_name(
     comment_id int auto_increment,
     passwd varchar(20) not null,
     typhoon_name varchar(20) not null,
     primary key (comment_id));
+
 
 /*Min Ji*/
 CREATE TABLE IF NOT EXISTS HUMIDTABLE (humiddate date NOT NULL, 
@@ -31,3 +30,28 @@ CREATE TABLE IF NOT EXISTS PRECIPITAIONTABLE (raindate date NOT NULL,
 
 CREATE TABLE IF NOT EXISTS UVTABLE (uvdate date NOT NULL, 
     region_code Foreign Key VARCHAR(4), insolation FLOAT(4), primary key(uvdate, region_code), foreign key(region_code));
+
+
+/*Hyuna*/
+CREATE TABLE REGION (
+	REGION_CODE CHAR(4) NOT NULL, 
+	ADMINISTRATIVE_AREA VARCHAR(20) NOT NULL,
+	CITY VARCHAR(20) NOT NULL,
+	PRIMARY KEY(REGION_CODE)
+);
+
+CREATE TABLE FINE_DUST (
+	DATE DATE NOT NULL
+	REGION_CODE CHAR(4) NOT NULL, 
+	FINE_DUST INT NOT NULL,
+	ULTRAFINE_DUST INT NOT NULL,
+	PRIMARY KEY(DATE, REGION_CODE)
+);
+
+CREATE TABLE OZONE (
+	DATE DATE NOT NULL
+	REGION_CODE CHAR(4) NOT NULL, 
+	OZONE FLOAT NOT NULL,
+	PRIMARY KEY(DATE, REGION_CODE)
+);
+
