58a9180713b124489f1cc3711bc13c099ce6c4c3
Hyuna Kim <akxmcse@gmail.com>
2022-11-16 01:33:27 +0900

    [ADD] drop.sql에 오존 평균 뷰 drop 추가

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,11 +1,20 @@
+/*Yeonhui*/
+drop table typhoon;
+drop table new_typhoon_name;
+drop table temperature;
+
 /*Min Ji*/
 drop table HUMIDTABLE;
 drop table UVTABLE;
 drop table PRECIPITAIONTABLE;
 
+/*Yeonsoo*/
+DROP TABLE sensory_temperature;
+DROP TABLE visibility;
+DROP TABLE tem_comment;
+
 /*Hyuna*/
-DROP TABLE REGION;
-
+DROP VIEW AVG_OZONE;
 DROP TABLE FINE_DUST;
-
 DROP TABLE OZONE;
+DROP TABLE REGION;
