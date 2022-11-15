448c7cb619ee74bd22f459629f05810b460ca376
Kim Hyuna <akxmcse@gmail.com>
2022-11-15 23:46:00 +0900

    [FIX] drop.sql 순서 변경

--- sql/drop.sql
+++ sql/drop.sql
@@ -3,14 +3,12 @@ drop table HUMIDTABLE;
 drop table UVTABLE;
 drop table PRECIPITAIONTABLE;
 
-/*Hyuna*/
-DROP TABLE REGION;
-
-DROP TABLE FINE_DUST;
-
-DROP TABLE OZONE;
-
 /*Yeonsoo*/
 DROP TABLE sensory_temperature;
 DROP TABLE visibility;
 DROP TABLE tem_comment;
+
+/*Hyuna*/
+DROP TABLE FINE_DUST;
+DROP TABLE OZONE;
+DROP TABLE REGION;
