088d195c9d2f31e8b71804222336ee824e6c9bfe
Hyuna Kim <akxmcse@gmail.com>
2022-11-16 20:30:57 +0900

    [FEAT] 유저 만족도 INSERT, UPDATE, DELETE 구현

--- webpage/hyuna/fine_particles.php
+++ webpage/hyuna/fine_particles.php
@@ -15,6 +15,9 @@
 </head>
 
 <body>
+    <?php
+    ini_set('display_errors', '0');
+    ?>
     <div class="grids">
         <div class="grid-header">
             <nav>
@@ -68,7 +71,7 @@
             $result2 = mysqli_query($conn, $query2);
             ?>
 
-            <FORM>
+            <FORM action="fine_particles.php" method="post">
                 <p>Fine dust Ranking of October</p>
                 <TABLE>
                     <thead>
@@ -116,13 +119,70 @@
                     </tr>
                     <?php } ?>
                 </TABLE>
+
+                <!-- 여기서부터 유저 만족도 테이블 -->
+
+                <p>Please leave your comment by satisfaction with the above information.</p>
+                <a>Nickname: <Input type="text" name="nickname"> Enter your Opinion: <Input type="text"
+                        name="satisfactionInsert"> <Input type="submit" value="Insert"></a>
+                <br>
+
+                <p>You can update or delete your opinion</p>
+                <a>Index: <Input type="text" name="updateIndex"> Edit your Opinion: <Input type="text" name="satisfactionUpdate">
+                    <Input type="submit" value="Update"></a> <br>
+                <a>Index: <Input type="text" name="deleteIndex"> Delete your Opinion <Input type="submit" value="Delete"></a>
+
+                <?php
+                if(empty($_POST['nickname']) || empty($_POST['satisfactionInsert'])){
+                    printf("Connect failed: %s\n",mysqli_connect_error());
+                } else {
+                    $insert = "INSERT INTO USER_SATISFACTION(NICKNAME, USER_SATISFACTION) VALUES('{$_POST['nickname']}','{$_POST['satisfactionInsert']}')";
+                    $result = mysqli_query($conn, $insert);    
+                }
+                
+                $update = "UPDATE USER_SATISFACTION SET USER_SATISFACTION = '{$_POST['satisfactionUpdate']}' WHERE USERID =  '{$_POST['updateIndex']}'";
+                $result = mysqli_query($conn, $update);
+
+                $delete = "DELETE FROM USER_SATISFACTION WHERE USERID = '{$_POST['deleteIndex']}'";
+                $result = mysqli_query($conn, $delete);
+
+                $query3 = "SELECT * FROM USER_SATISFACTION;";
+                $result3 = mysqli_query($conn, $query3);
+                ?>
+
+                <TABLE>
+                    <thead>
+                        <tr>
+                            <td>Index</td>
+                            <td>Nickname</td>
+                            <td>Opinion</td>
+                        </tr>
+                    </thead>
+                    <?php while ($row = mysqli_fetch_array($result3)) { ?>
+                    <tr>
+                        <td>
+                            <?= $row['USERID'] ?>
+                        </td>
+                        <td>
+                            <?= $row['NICKNAME'] ?>
+                        </td>
+                        <td>
+                            <?= $row['USER_SATISFACTION'] ?>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </TABLE>
+                <!-- 여기까지 유저 만족도 테이블 -->
+
             </FORM>
             <?php
             mysqli_free_result($result1);
             mysqli_free_result($result2);
+            mysqli_free_result($result3); //유저 만족도 테이블 SELECT에 대한 FREE
             ?>
 
-            <button type="button" class="userBtn" onclick="location.href='fine_particles_trend.php'">See the concentration trend of
+            <button type="button" class="userBtn" onclick="location.href='fine_particles_trend.php'">See the
+                concentration trend of
                 October</a>
         </div>
         <div class="grid-footer">
