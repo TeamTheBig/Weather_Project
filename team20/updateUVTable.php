ddc3929877920f8ea68389cc65b6d3c4c1eb0d8c
Kang1221 <90603399+Kang1221@users.noreply.github.com>
2022-11-17 20:52:37 +0900

    [Minji] add insert function

--- team20/updateUVTable.php
+++ team20/updateUVTable.php
@@ -12,32 +12,47 @@
             </div>
         </div>
         <?php
-            //include_once ("login.php");
-            $host = 'localhost';
-            $user = 'team20';
-            $pw = 'team20';
-            $db = 'team20';
+           $host = 'localhost';
+           $user = 'team20';
+           $pw = 'team20';
+           $db = 'team20';
 
 
-            $mysqli = new mysqli($host, $user, $pw, $db);
+           $mysqli = mysqli_connect($host, $user, $pw, $db);
 
-            //사용자로부터 date, region code, insolation 값을 입력받음.
-            $updateday = $_POST["updatedate"];
-            $updateregion = $_POST['updateregion'];
-            $updateinsolation = $_POST['updateinsolation'];
+           if ($mysqli === false) {
+               die("ERROR: Could not connect. " . mysqli_connect_error());
+           }
 
-            //UPDATE
-            //값이 모두 입력되었으면 테이블 값을 업데이트
-            if(isset($updateinsolation) && isset($updateday) && isset($updateregion)){
-                settype($updateinsolation, 'float');
-                $sql = "UPDATE uvtable set insolation = '".$updateinsolation."' where uvdate = '".$updateday."' and region_code = '".$updateregion."'";
-                $res = mysqli_query($mysqli, $sql); 
-                //echo "Successful";
-            }
-            else{
-                $sql = "INSERT INTO uvtable (uvdate, region_code, insolation) VALUES ('".$updateday."', '".$updateregion."', '".$updateinsolation."')";
-                $res = mysqli_query($mysqli, $sql);
-            }
+           //사용자로부터 date, region code, insolation 값을 입력받음.
+           $updateday = $_POST["updatedate"];
+           $updateregion = $_POST["updateregion"];
+           $updateinsolation = $_POST["updateinsolation"];
+
+           
+           //사용자의 입력값에 대한 insolation이 존재하는지 확인
+           $sql = "SELECT insolation FROM uvtable WHERE uvdate = '".$updateday."' and region_code = '".$updateregion."'";
+           $res = mysqli_query($mysqli, $sql);
+           $data = mysqli_fetch_array($res);
+           //Update
+           //데이터가 테이블에 존재하면 update
+           if(isset($data)) {
+               $sql1 = "UPDATE uvtable set insolation = '".$updateinsolation."' where uvdate = '".$updateday."' and region_code = '".$updateregion."'";
+
+               $res = mysqli_query($mysqli, $sql1);
+               echo "<script>alert('successfully updated');</script>";
+
+              
+           }
+           //Insert
+           //값이 모두 입력되었으면 테이블 값을 업데이트
+           else{
+               $sql2 = "INSERT INTO uvtable(uvdate, region_code, insolation) VALUES ('".$updateday."', '".$updateregion."', '".$updateinsolation."')";
+               //"UPDATE uvtable set insolation = '".$updateinsolation."' where uvdate = '".$updateday."' and region_code = ''";
+               $res = mysqli_query($mysqli, $sql2);
+               echo "<script>alert('success inserted');</script>";
+           }
+           mysqli_close($mysqli);
         ?>      
          <!--사용자의 input을 통해 삭제한 경우 뜨는 문구-->
         <div class="success">SUCCESSFULLY INSERTED OR UPDATED</div>
